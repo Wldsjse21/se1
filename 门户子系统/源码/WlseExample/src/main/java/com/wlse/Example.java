65f7223a62069f12064f35af67c86e4376b8e0ab
coughs <conghaishan@dingtalk.com>
2021-10-23 14:24:23 +0800

    A+B 提交第一版。
    编译信息
    编译失败
    
                /tmp/compiler_hyv0r57n/Main.java:5: 错误: 类Plus是公共的, 应在名为
    Plus.java 的文件中声明
    public class Plus {
           ^
    /tmp/compiler_hyv0r57n/Main.java:15: 错误: 不兼容的类型: 意外的返回值
    		return a + b;
    		         ^
    2 个错误

--- 门户子系统/源码/WlseExample/src/main/java/com/wlse/Example.java
+++ 门户子系统/源码/WlseExample/src/main/java/com/wlse/Example.java
@@ -4,22 +4,16 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class Example {
-	public static void main(String[] arg) {
+public class Plus {
+	public static void main() {
+		int sum = 0;
 		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-        String inStr = "";
-
-		try {
-			int a = 0;
-			int b = 0;
-			inStr = in.readLine();
-			String[] params = inStr.split(" "); 
-			a = Integer.parseInt(params[0]);
-			b = Integer.parseInt(params[1]);
-	        System.out.print(a + b);
-		} catch (NumberFormatException | IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+		int a = 0;
+		int b = 0;
+		
+		a = new Integer(in.readLine());
+		b = new Integer(in.readLine());
+		
+		return a + b;
 	}
 }
