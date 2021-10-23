0a12f7aa0fa580133f88109fd2be4eefd0f6b851
coughs <conghaishan@dingtalk.com>
2021-10-23 13:27:45 +0800

    Simple Example SE1。

---
+++ 门户子系统/源码/WlseExample/src/main/java/com/wlse/Example.java
@@ -0,0 +1,14 @@
+package com.wlse;
+
+public class Example {
+	public static void main(String[] arg) {
+	}
+
+	public int add(int op1, int op2) {
+		return op1 + op2;
+	}
+
+	public int sub(int op1, int op2) {
+		return op1 - op2;
+	}
+}
