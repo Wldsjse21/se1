0fb569a003fc8fbdc90fad721c6b44f0a02855b2
coughs <conghaishan@dingtalk.com>
2021-10-23 15:43:36 +0800

    精简版本的A+B。

--- 门户子系统/源码/WlseExample/src/main/java/com/wlse/Example.java
+++ 门户子系统/源码/WlseExample/src/main/java/com/wlse/Example.java
@@ -1,23 +1,21 @@
+package com.wlse;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class Main {
+public class Example {
 	public static void main(String[] arg) {
 		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-        String inStr = "";
+		int a = 0;
+		int b = 0;
 
 		try {
-			int a = 0;
-			int b = 0;
-			inStr = in.readLine();
-			String[] params = inStr.split(" "); 
+			String[] params = in.readLine().split(" "); 
 			a = Integer.parseInt(params[0]);
 			b = Integer.parseInt(params[1]);
 	        System.out.print(a + b);
 		} catch (NumberFormatException | IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
 		}
 	}
 }
