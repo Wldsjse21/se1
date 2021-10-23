0203b59859abf3df35112dffcf4faa16c012572d
coughs <conghaishan@dingtalk.com>
2021-10-23 14:12:53 +0800

    AC

--- 门户子系统/源码/WlseExample/src/main/java/com/wlse/Example.java
+++ 门户子系统/源码/WlseExample/src/main/java/com/wlse/Example.java
@@ -22,12 +22,4 @@ public class Example {
 			e.printStackTrace();
 		}
 	}
-
-	public int add(int op1, int op2) {
-		return op1 + op2;
-	}
-
-	public int sub(int op1, int op2) {
-		return op1 - op2;
-	}
 }
