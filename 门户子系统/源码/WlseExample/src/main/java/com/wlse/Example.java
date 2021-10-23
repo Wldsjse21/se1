979a03337fa549702de9618c6b6ddd654eb0b2a2
coughs <conghaishan@dingtalk.com>
2021-10-23 13:34:31 +0800

    AC a+b

--- 门户子系统/源码/WlseExample/src/main/java/com/wlse/Example.java
+++ 门户子系统/源码/WlseExample/src/main/java/com/wlse/Example.java
@@ -1,7 +1,26 @@
 package com.wlse;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Example {
 	public static void main(String[] arg) {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        String inStr = "";
+
+		try {
+			int a = 0;
+			int b = 0;
+			inStr = in.readLine();
+			String[] params = inStr.split(" "); 
+			a = Integer.parseInt(params[0]);
+			b = Integer.parseInt(params[1]);
+	        System.out.print(a + b);
+		} catch (NumberFormatException | IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 	public int add(int op1, int op2) {
