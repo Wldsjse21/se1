d7f03cf85c99aef360f577a2dfd688808802d0cb
coughs <conghaishan@dingtalk.com>
2021-10-24 14:45:33 +0800

    更新输出（String）。

--- 例/WlseExample/src/main/java/com/wlse/Example.java
+++ 例/WlseExample/src/main/java/com/wlse/Example.java
@@ -1,21 +1,26 @@
 package com.wlse;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 
 public class Example {
 	public static void main(String[] arg) {
 		BufferedReader in = new BufferedReader(
 				new InputStreamReader(System.in));
+		BufferedWriter out = new BufferedWriter(
+				new OutputStreamWriter(System.out));
 		int a = 0, b = 0;
 
 		try {
 			String[] params = in.readLine().split(" ");
 			a = Integer.parseInt(params[0]);
 			b = Integer.parseInt(params[1]);
-			System.out.print(a + b);
-		} catch (NumberFormatException | IOException e) {
+			Integer ans = a + b;
+			out.write(ans.toString());
+		} catch (IOException e) {
 		}
 	}
 }
