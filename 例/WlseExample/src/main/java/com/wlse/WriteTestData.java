8c4765a3ff3519f96aedf1dcb714456b90b09ded
coughs <conghaishan@dingtalk.com>
2021-10-24 14:35:45 +0800

    增加写测试数据。

---
+++ 例/WlseExample/src/main/java/com/wlse/WriteTestData.java
@@ -0,0 +1,29 @@
+package com.wlse;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Random;
+
+public class WriteTestData {
+	final static int N = 10000;
+	final static int Z_Len = 0;
+
+	public static void main(final String args[]){
+		try {
+			final BufferedWriter out = new BufferedWriter(
+					new FileWriter("Calculator.in"));
+			
+			final Random r = new Random();
+			Integer ii = 0;
+			ii = Math.abs(r.nextInt()%N);
+			out.write(ii.toString());
+			out.write(" ");
+			ii = Math.abs(r.nextInt()%N);
+			out.write(ii.toString());
+
+			out.close();
+		} catch (final IOException e) {
+		}
+	}
+}
