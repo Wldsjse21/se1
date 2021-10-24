4b413c32efdc2c04637b9011397c5a9fb54cfa53
coughs <conghaishan@dingtalk.com>
2021-10-24 16:13:35 +0800

    改成使用文件输入输出。

--- 例/WlseExample/src/main/java/com/wlse/Example.java
+++ 例/WlseExample/src/main/java/com/wlse/Example.java
@@ -2,24 +2,29 @@ package com.wlse;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 
 public class Example {
 	public static void main(String[] arg) {
-		BufferedReader in = new BufferedReader(
-				new InputStreamReader(System.in));
-		BufferedWriter out = new BufferedWriter(
-				new OutputStreamWriter(System.out));
-		int a = 0, b = 0;
-
 		try {
+			BufferedReader in = new BufferedReader(
+					new FileReader("Example.in"));
+			BufferedWriter out = new BufferedWriter(
+					new FileWriter("Example.out"));
+			int a = 0, b = 0;
+
 			String[] params = in.readLine().split(" ");
 			a = Integer.parseInt(params[0]);
 			b = Integer.parseInt(params[1]);
 			Integer ans = a + b;
 			out.write(ans.toString());
+
+			in.close();
+			out.close();
+		} catch (FileNotFoundException e1) {
 		} catch (IOException e) {
 		}
 	}
